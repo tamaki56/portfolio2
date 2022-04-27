d4d26052400108c138aef6de1246aa1d373b0d56
tamaki56 <http56@icloud.com>
2022-04-27 13:24:35 +0000

    [add]photo edit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,7 +7,6 @@ class PhotosController < ApplicationController
 
   def create
     @photo = current_user.photos.new(photo_params)
-
     if @photo.save
       @photo.save_tags(params[:photo][:tags][:tag])
       redirect_to photo_path(@photo.id)
@@ -25,6 +24,27 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
     if @photo.destroy
       redirect_to user_path(current_user)
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+    @user = @photo.user
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      # タグの更新
+      @photo.save_tags(params[:photo][:tags][:tag])
+      # 成功したら投稿記事へリダイレクト
+      redirect_to photo_path(@photo.id)
+    else
+      # 失敗した場合は、editへ戻る
+      render :edit
     end
   end
 
