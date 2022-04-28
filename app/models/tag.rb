dd90216aa80c95e691d9d0763a7dd91a55212990
tamaki56 <http56@icloud.com>
2022-04-28 23:42:09 +0000

    [add]レスポンシブ

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,5 @@ class Tag < ApplicationRecord
   # タグは複数の投稿を持つ　それは、post_tagsを通じて参照できる
   has_many :photos,through: :photo_tags
 
-  validates :name, uniqueness: true, presence: true  
+  validates :name, uniqueness: true, presence: true
 end
